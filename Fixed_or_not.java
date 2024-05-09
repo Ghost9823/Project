7ee6f505e57bda0ecac8dd18377d51a801e2e217
Ghost9823 <sushantdanghi@gmail.com>
2024-05-09 21:33:22 +0530

    maybe

---
+++ Fixed_or_not.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Fixed_or_not {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scn.nextInt();
+            int count = 0;
+            for (int div = 2; div * div <= n; div++) {
+                if (n % div == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not Prime");
+            }
+        }
+        scn.close(); // Closing the scanner to prevent resource leak
+    }
+}
