4093f8ed40f18aacb45d0e18ef2ff26043f3f72c
Ghost9823 <sushantdanghi@gmail.com>
2024-05-10 19:13:03 +0530

    Day 2

--- Main.java
+++ Main.java
@@ -1,26 +1,26 @@
-import java.util.*;
 import java.util.Scanner;
-public class Main(
+public class Main{
     public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        
-        int count = 0;
-        for(int i = 0;i < t;i++){
-            int n = scn.nextInt();
-            count = 0;
-            for(int div = 2;div * div <= n;div++){
-                if(n % div == 0){
-                    count++;
-                    break;
+        try (Scanner scn = new Scanner(System.in)) {
+            int t = scn.nextInt();
+            
+            int count = 0;
+            for(int i = 0;i < t;i++){
+                int n = scn.nextInt();
+                count = 0;
+                for(int div = 2;div * div <= n;div++){
+                    if(n % div == 0){
+                        count++;
+                        break;
+                    }
                 }
-            }
-            if (count == 0){
-                System.out.println("Prime");    
-            }
-            else{
-                System.out.println("Not Prime");
+                if (count == 0){
+                    System.out.println("Prime");    
+                }
+                else{
+                    System.out.println("Not Prime");
+                }
             }
         }
     }
-)
+}
---
+++ Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    int age = 20;
+    public static void main(String[] args) {
+        Person rohan = new Person();
+        System.out.println(rohan.age);
+    
+    }
+
+
+}
