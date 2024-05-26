8cebb17193762662d9a6ec427a8d127455ba6b62
Ghost9823 <sushantdanghi@gmail.com>
2024-05-26 09:46:05 +0530

    You can have more than one methods in public check this

--- Array.java
+++ Array.java
@@ -1,20 +1,19 @@
 public class Array {
-    void linearSearch() {
-        int[] arr = {1,2,3,4,5};
-        int x = 3;
+    public void linearSearch(int[] arr, int x) {
         int ans = -1;
-        for(int i = 0;i < arr.length;i++){
-            if (arr[i]==x){
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] == x){
                 System.out.println(x + " Element found at index: " + i);
-                break;
-            } else {
-                System.out.println(ans);
+                return;
             }
         }
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
         Array obj = new Array();
-        obj.linearSearch();
+        int[] arr = {1, 2, 3, 4, 5};
+        int x = 3;
+        obj.linearSearch(arr, x);
     }
 }
---
+++ Occurence.java
@@ -0,0 +1,16 @@
+public class Occurence {
+    public static void LastOccurence0(int[] arr, int x) {
+        Hella(arr, x);
+        System.out.println("read");
+    }
+    
+    public static void Hella(int[] arr_2, int y) {
+        // Add your implementation here
+    }    
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int x = 3;
+        LastOccurence0(arr, x);
+    }
+}
